2cdd68e42fb554416def8ec00947f849e9ccb238
SR Sajjad <sajjadsr001@gmail.com>
2024-11-20 22:16:52 +0600

    add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,34 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const clashDisplay = localFont({
+  src: [
+    {
+      path: "./fonts/WEB/fonts/ClashDisplay-Light.woff",
+      weight: "300",
+    },
+    {
+      path: "./fonts/WEB/fonts/ClashDisplay-Regular.woff",
+      weight: "400",
+    },
+    {
+      path: "./fonts/WEB/fonts/ClashDisplay-Medium.woff",
+      weight: "500",
+    },
+    {
+      path: "./fonts/WEB/fonts/ClashDisplay-Semibold.woff",
+      weight: "600",
+    },
+    {
+      path: "./fonts/WEB/fonts/ClashDisplay-Bold.woff",
+      weight: "700",
+    },
+    {
+      path: "./fonts/WEB/fonts/ClashDisplay-Extralight.woff",
+      weight: "800",
+    },
+  ],
+  variable: "--font-clash-display",
 });
 
 export default function RootLayout({
@@ -20,11 +39,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${clashDisplay.variable} antialiased`}>{children}</body>
     </html>
   );
 }
